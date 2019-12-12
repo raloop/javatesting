2c8859c17cc02d0b53dcbc51ec29253bd7dfa52c
Rares Lup <luprares@gmail.com>
2019-12-12 18:54:39 +0200

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		in.close();
+		
+		int[] grades; 
+		int scores[];
+		
+		
+	}
+}
