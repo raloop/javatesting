c37ad31b46227e6e29b9e68f601c9f2742fb7a54
Rares Lup <luprares@gmail.com>
2019-12-13 18:26:52 +0200

    Convert queue to stack

--- src/Test.java
+++ src/Test.java
@@ -1,24 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner in = new Scanner(new File("students.txt"));
 		
-		List<String> students = new ArrayList<String>();
+		LinkedList<String> names = new LinkedList<String>();
+		names.push("Caleb");
+		names.push("Sue");
+		names.push("Sally");
 		
-		while(in.hasNextLine()) {
-			students.add(in.nextLine());
-		}
-		
-		for (int i = 0; i < students.size(); i++) {
-			System.out.println("Name " + students.get(i));
-		}
-		
-		in.close();
+		System.out.println(names.pop());
+		System.out.println(names.remove());
+		System.out.println(names.remove());
 	}
 }
