685ecc222a060708bac5a5d16eeee8be0dcbb267
Rares Lup <luprares@gmail.com>
2019-12-13 19:29:09 +0200

    Iterators

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,6 @@
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.ListIterator;
 
 public class Test {
 
@@ -14,8 +11,20 @@ public class Test {
 		names.push("Sue");
 		names.push("Sally");
 		
-		System.out.println(names.pop());
-		System.out.println(names.remove());
-		System.out.println(names.remove());
+		ListIterator<String> it = names.listIterator();
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
+		for (String s : names) {
+			System.out.println(s);
+		}
+		
+		names.add(2, "Susan");
+		
+		for (String s : names) {
+			System.out.println("The names is " + s);
+		}
 	}
 }
